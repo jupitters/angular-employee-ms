0018ad11b28be1f6cfd5344a2a28bb6b0ec3da44
jupitters <sampaiobale@gmail.com>
2026-01-27 18:52:58 -0300

    get all employees

--- angular-frontend/src/app/app.ts
+++ angular-frontend/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {EmployeeList} from './employee-list/employee-list';
+import {HttpClientModule} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, EmployeeList],
+  imports: [RouterOutlet, EmployeeList, HttpClientModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
