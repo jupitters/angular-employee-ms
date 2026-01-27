f4256d4ee1bb5a6714b14fb649ce5fd90b264e9b
jupitters <sampaiobale@gmail.com>
2026-01-27 19:02:59 -0300

    routes

--- angular-frontend/src/app/app.ts
+++ angular-frontend/src/app/app.ts
@@ -2,10 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {EmployeeList} from './employee-list/employee-list';
 import {HttpClientModule} from '@angular/common/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppRoutes} from './app.routes';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, EmployeeList, HttpClientModule],
+  imports: [RouterOutlet, EmployeeList, HttpClientModule, BrowserModule, AppRoutes],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
