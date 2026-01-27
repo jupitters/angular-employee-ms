4884f424d57553286fd65154e70e1a85459e4ddb
jupitters <sampaiobale@gmail.com>
2026-01-27 18:27:18 -0300

    employee list

--- angular-frontend/src/app/employee-list/employee-list.ts
+++ angular-frontend/src/app/employee-list/employee-list.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import {Employee} from '../employee';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-employee-list',
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './employee-list.html',
   styleUrl: './employee-list.css',
 })
 export class EmployeeList {
-
+  employees: Employee[] | undefined;
 }
