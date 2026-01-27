2747fcd2c1b9986d06a7d3b566f6669c269c0d37
jupitters <sampaiobale@gmail.com>
2026-01-27 17:16:40 -0300

    employee model

---
+++ src/main/java/com/jupitters/employee_ms/model/Employee.java
@@ -0,0 +1,20 @@
+package com.jupitters.employee_ms.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String function;
+}
