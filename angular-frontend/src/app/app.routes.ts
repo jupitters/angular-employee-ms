3964c807799757b416d0790d1ca2dddc85ef8aef
jupitters <sampaiobale@gmail.com>
2026-01-27 19:08:43 -0300

    paths

--- angular-frontend/src/app/app.routes.ts
+++ angular-frontend/src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import {RouterModule, Routes} from '@angular/router';
 import {NgModule} from '@angular/core';
+import {EmployeeList} from './employee-list/employee-list';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'employees', component: EmployeeList},
+  {path: '', redirectTo: 'employees', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
