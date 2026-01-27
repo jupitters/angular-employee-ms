90d45ecad8688d9173eaaf8a6eba0644ed468792
jupitters <sampaiobale@gmail.com>
2026-01-27 19:29:43 -0300

    fixing routes

--- angular-frontend/src/app/app.routes.ts
+++ angular-frontend/src/app/app.routes.ts
@@ -1,15 +1,7 @@
 import {RouterModule, Routes} from '@angular/router';
-import {NgModule} from '@angular/core';
 import {EmployeeList} from './employee-list/employee-list';
 
-const routes: Routes = [
+export const routes: Routes = [
   {path: 'employees', component: EmployeeList},
   {path: '', redirectTo: 'employees', pathMatch: 'full'}
 ];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-
-export class AppRoutes{}
--- angular-frontend/src/app/app.ts
+++ angular-frontend/src/app/app.ts
@@ -1,13 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {EmployeeList} from './employee-list/employee-list';
-import {HttpClientModule} from '@angular/common/http';
 import {BrowserModule} from '@angular/platform-browser';
-import {AppRoutes} from './app.routes';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, EmployeeList, HttpClientModule, BrowserModule, AppRoutes],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
