a2f6e53e85faf6dcc50f0ec456d41479f9bcda8d
jupitters <sampaiobale@gmail.com>
2026-01-27 17:25:17 -0300

    employee controller

--- src/main/java/com/jupitters/employee_ms/controller/EmployeeController.java
+++ src/main/java/com/jupitters/employee_ms/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import com.jupitters.employee_ms.model.Employee;
 import com.jupitters.employee_ms.repository.EmployeeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,7 @@ import java.util.List;
 public class EmployeeController {
     private final EmployeeRepository employeeRepository;
 
+    @GetMapping("/employees")
     public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
     }
