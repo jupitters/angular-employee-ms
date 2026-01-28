0864875410701fef0de1715f994a856bf27d45a5
jupitters <sampaiobale@gmail.com>
2026-01-28 17:27:19 -0300

    create employee

--- angular-frontend/src/app/create-employee/create-employee.ts
+++ angular-frontend/src/app/create-employee/create-employee.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Employee} from '../employee';
 
 @Component({
   selector: 'app-create-employee',
@@ -7,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrl: './create-employee.css',
 })
 export class CreateEmployee {
+  employee: Employee = new Employee();
 
+  constructor() {
+  }
 }
