5bb5966280d7424e5b7411ccf6baaf2a94ad7034
jupitters <sampaiobale@gmail.com>
2026-01-29 12:24:56 -0300

    update employee component

---
+++ angular-frontend/src/app/update-employee/update-employee.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-employee',
+  imports: [],
+  templateUrl: './update-employee.html',
+  styleUrl: './update-employee.css',
+})
+export class UpdateEmployee {
+
+}
