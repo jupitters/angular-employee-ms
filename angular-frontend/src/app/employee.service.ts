a236e6b8c9174221849e0f83501774aa558962ba
jupitters <sampaiobale@gmail.com>
2026-01-29 13:03:32 -0300

    get employee by id

--- angular-frontend/src/app/employee.service.ts
+++ angular-frontend/src/app/employee.service.ts
@@ -17,4 +17,8 @@ export class EmployeeService {
   createEmployee(employee: Employee): Observable<Object>{
     return this.httpClient.post(`${this.baseUrl}`, employee);
   }
+
+  getEmployeeById(id: number): Observable<Employee>{
+    return this.httpClient.get<Employee>(`${this.baseUrl}/${id}`);
+  }
 }
