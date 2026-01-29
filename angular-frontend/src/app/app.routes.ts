8ce2dfbafd687a570f14ac5a8f4775619f66c5b5
jupitters <sampaiobale@gmail.com>
2026-01-29 12:26:35 -0300

    update employee route

--- angular-frontend/src/app/app.routes.ts
+++ angular-frontend/src/app/app.routes.ts
@@ -6,6 +6,6 @@ import {UpdateEmployee} from './update-employee/update-employee';
 export const routes: Routes = [
   {path: 'employees', component: EmployeeList},
   {path: 'create-employee', component: CreateEmployee},
-  {path: 'update-employee', component: UpdateEmployee},
+  {path: 'update-employee/:id', component: UpdateEmployee},
   {path: '', redirectTo: 'employees', pathMatch: 'full'}
 ];
