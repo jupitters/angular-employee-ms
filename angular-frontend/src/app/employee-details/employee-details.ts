4a852eb923087767ee5d3a433900cae610a721c7
jupitters <sampaiobale@gmail.com>
2026-01-30 11:21:26 -0300

    employee details

--- angular-frontend/src/app/employee-details/employee-details.ts
+++ angular-frontend/src/app/employee-details/employee-details.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Employee} from '../employee';
 
 @Component({
   selector: 'app-employee-details',
@@ -6,6 +7,18 @@ import { Component } from '@angular/core';
   templateUrl: './employee-details.html',
   styleUrl: './employee-details.css',
 })
-export class EmployeeDetails {
+export class EmployeeDetails implements OnInit{
 
+  id: number | undefined;
+  employee: Employee | undefined;
+  constructor(){}
+
+  ngOnInit():void{
+    this.id = this.route.snapshot.params['id'];
+
+    this.employee = new Employee();
+    this.employeeService.getEmployeeByID(this.id).subscribe(data => {
+      this.employee = data;
+    });
+  }
 }
