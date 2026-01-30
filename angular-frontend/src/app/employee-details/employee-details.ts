96ea931d2a12d3a5282f27d7b80dabc19902391a
jupitters <sampaiobale@gmail.com>
2026-01-30 11:23:20 -0300

    employee details

--- angular-frontend/src/app/employee-details/employee-details.ts
+++ angular-frontend/src/app/employee-details/employee-details.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Employee} from '../employee';
+import {EmployeeService} from '../employee.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-employee-details',
@@ -9,15 +11,15 @@ import {Employee} from '../employee';
 })
 export class EmployeeDetails implements OnInit{
 
-  id: number | undefined;
+  id: number;
   employee: Employee | undefined;
-  constructor(){}
+  constructor(private route: ActivatedRoute, private employeeService: EmployeeService){}
 
   ngOnInit():void{
     this.id = this.route.snapshot.params['id'];
 
     this.employee = new Employee();
-    this.employeeService.getEmployeeByID(this.id).subscribe(data => {
+    this.employeeService.getEmployeeById(this.id).subscribe(data => {
       this.employee = data;
     });
   }
